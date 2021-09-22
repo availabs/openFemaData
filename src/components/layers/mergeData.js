1b77167284473f168cf8b7f969e64a356929a289
shaunaksangdod <shaunaksangdod@gmail.com>
2021-09-22 18:57:23 -0400

    merge

---
+++ src/components/layers/mergeData.js
@@ -0,0 +1,234 @@
+import {scaleLinear} from "d3-scale"
+import _ from "lodash"
+import get from "lodash.get"
+import {LayerContainer} from "@availabs/avl-map"
+import {getColorRange, useTheme} from "@availabs/avl-components";
+import {fnum} from "../../utils/fnum";
+import {ckmeans} from 'simple-statistics'
+
+const mapping = {
+    'Severe Weather': 'swd',
+    'Open Fema': 'ofd',
+    'Difference (swd - ofd)': 'Difference'
+}
+class mergeData extends LayerContainer {
+
+    // setActive = !!this.viewId
+    name = 'Merge Data'
+    id = 'Merge Data'
+    data = []
+    filters = {
+        dataset: {
+            name: "Dataset",
+            type: "dropdown",
+            multi: false,
+            value: ['Severe Weather'],
+            domain: ['Severe Weather', 'Open Fema', 'Difference (swd - ofd)'],
+        },
+        year: {
+            name: "Year",
+            type: "dropdown",
+            multi: false,
+            value: 'All Time',
+            domain: [],
+        },
+        hazard: {
+            name: "Hazard",
+            type: "dropdown",
+            multi: false,
+            value: 'All Hazards',
+            domain: [],
+        },
+    }
+
+    legend = {
+        Title: ({layer}) => get(layer.filters.attribute, 'value', '').replace(/_/g, ' '),
+        type: "quantile",
+        domain: [],
+        format: fnum,
+        range: getColorRange(7, "YlOrRd", true),
+        show: true,
+    }
+
+    onHover = {
+        layers: ['counties'],
+        HoverComp: ({data, layer}) => {
+            const theme = useTheme();
+            return (
+                <div style={{maxHeight: '300px'}} className={`${theme.bg} rounded relative px-1 overflow-auto scrollbarXsm`}>
+                    {
+                        data.map((row, i) =>
+                            <div key={i} className="flex">
+                                {
+                                    row.map((d, ii) =>
+                                        <div key={ii}
+                                             style={{maxWidth: '200px'}}
+                                             className={`
+                                                    ${ii === 0 ? "flex-1 font-bold" : "overflow-auto scrollbarXsm"}
+                                                    ${row.length > 1 && ii === 0 ? "mr-4" : ""}
+                                                    ${row.length === 1 && ii === 0 ? `border-b-2 text-lg ${i > 0 ? "mt-1" : ""}` : ""}
+                                                    `}>
+                                            {d}
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
+                </div>
+            )
+        },
+        callback: (layerId, features, lngLat) => {
+
+            return features.reduce((a, feature) => {
+                let data = get(this.data.filter(d => d.geoid === feature.properties.geoid), [0], {})
+
+                return [
+                    ...a,
+                    [get(this.geoNames, [feature.properties.geoid, 'name'], feature.properties.geoid)],
+                    ...Object.keys(data)
+                        .filter(d => !['geoid', 'group_by'].includes(d))
+                        .reduce((acc, curr) => {
+                            acc.push([curr.replace(/_/g, ' '), parseFloat(data[curr]).toLocaleString()])
+                            return acc;
+                        }, [])
+                ]
+            }, []);
+        }
+    }
+
+    sources = [
+        {
+            id: "counties",
+            source: {
+                'type': "vector",
+                'url': 'mapbox://am3081.a8ndgl5n'
+            },
+        },
+    ]
+
+    layers = [
+        {
+            'id': 'counties',
+            'source': 'counties',
+            'source-layer': 'counties',
+            'type': 'fill',
+        },
+        {
+            'id': 'counties-line',
+            'source': 'counties',
+            'source-layer': 'counties',
+            'type': 'line',
+            paint: {
+                "line-width": [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    4, 0,
+                    22, 0
+                ],
+                "line-color": '#ccc',
+                "line-opacity": 0.5
+            }
+        }
+    ]
+
+
+    init(map, falcor) {
+        map.fitBounds([-125.0011, 24.9493, -66.9326, 49.5904])
+    }
+
+    onFilterChange(filterName, newValue, prevValue) {
+        // this.legend.format = newValue === 'num_valid_registrations' ? d => fnum(d, false) : fnum;
+    }
+
+    receiveProps(props, map, falcor, MapActions) {
+        this.fetchData(falcor).then(() => this.render(map, falcor))
+    }
+
+    fetchData(falcor) {
+        return falcor.get(
+            ['swdOfdMerge', 'indexValues', ['geoid', 'hazard', 'year']],
+            ['swdOfdMerge', 'swd', 'geoid'],
+            ['swdOfdMerge', 'swd', 'geoid.hazard'],
+            ['swdOfdMerge', 'swd', 'geoid.year'],
+            ['swdOfdMerge', 'swd', 'geoid.hazard.year'],
+            ['swdOfdMerge', 'ofd', 'geoid'],
+            ['swdOfdMerge', 'ofd', 'geoid.hazard'],
+            ['swdOfdMerge', 'ofd', 'geoid.year'],
+            ['swdOfdMerge', 'ofd', 'geoid.hazard.year'],
+        ).then(d => {
+            this.data = get(d, 'json.swdOfdMerge', {})
+            this.filters.year.domain = ['All Time', ...get(d, 'json.swdOfdMerge.indexValues.year', [])]
+            this.filters.hazard.domain = ['All Hazards', ...get(d, 'json.swdOfdMerge.indexValues.hazard', [])]
+        }).then(() => {
+            return _.chunk(get(this.data, 'indexValues.geoid', ['36']).filter(f => f !== 'None'), 100)
+                .reduce((acc, curr) => falcor.get(['geo', curr, 'name']), Promise.resolve())
+        }).then(names => {
+            this.geoNames = get(falcor.getCache(), ['geo'], {})
+        })
+    }
+
+    getColorScale(domain) {
+        if (this.legend.range.length > domain.length) return this.legend.domain = []
+
+        this.legend.domain = ckmeans(domain, this.legend.range.length).map(d => Math.min(...d))
+
+        return scaleLinear()
+            .domain(this.legend.domain)
+            .range(this.legend.range);
+    }
+
+    paintMap(map, data) {
+        const colorScale = this.getColorScale(data.map(d => +(d.total_damage || d.total_loss)));
+        let colors = {};
+
+        data.forEach(d => {
+            colors[d.geoid] = colorScale(+(d.total_damage || d.total_loss));
+        });
+        map.setPaintProperty('counties', 'fill-color',
+            ['get', ['get', 'geoid'], ['literal', colors]]);
+    }
+
+    render(map, falcor) {
+        
+        let grouping =
+            this.filters.hazard.value === 'All Hazards' && this.filters.year.value === 'All Time' ? 'geoid' :
+            this.filters.hazard.value === 'All Hazards' && this.filters.year.value !== 'All Time' ? 'geoid.year' :
+            this.filters.hazard.value !== 'All Hazards' && this.filters.year.value === 'All Time' ? 'geoid.hazard' :
+            this.filters.hazard.value !== 'All Hazards' && this.filters.year.value !== 'All Time' ? 'geoid.hazard.year' : 'geoid'
+
+        let tmpData = []
+        if (mapping[this.filters.dataset.value] === 'Difference'){
+            let swd = this.data['swd'][grouping],
+                ofd = this.data['ofd'][grouping];
+
+            get(this.data, 'indexValues.geoid', [])
+                .forEach(geoid => {
+                    let swdLoss = get(swd.filter(s => s.geoid === geoid), [0, 'total_damage'], 0),
+                        ofdLoss = get(ofd.filter(o => o.geoid === geoid), [0, 'total_loss'], 0)
+                    tmpData.push(
+                        {
+                            geoid,
+                            swdLoss,
+                            ofdLoss,
+                            total_loss: swdLoss - ofdLoss
+                        }
+                    )
+                })
+
+        }else{
+            tmpData = this.data[mapping[this.filters.dataset.value]][grouping]
+        }
+        this.data =
+            grouping.split('.')
+                .filter(g => g !== 'geoid')
+                .reduce((acc, curr) =>
+                    acc.filter(a => a[curr] === this.filters[curr].value)
+                , tmpData)
+
+        this.paintMap(map, this.data);
+    }
+}
+
+export const MergeDataFactory = (options = {}) => new mergeData(options)
--- src/pages/Home/merge.js
+++ src/pages/Home/merge.js
@@ -1,11 +1,52 @@
-import React, {useState} from "react"
+import React from "react"
 import get from 'lodash.get'
 import {useFalcor} from '@availabs/avl-components'
-import {BarGraph, generateTestBarData} from '../../components/avl-graph/src'
+import {BarGraph} from '../../components/avl-graph/src'
 import AdminLayout from '../Layout'
 import {fnum} from "utils/fnum";
+import { RenderMap } from "./components/merge/map";
 
-const ATTRIBUTES = ['year', 'geoid', 'hazard', 'group_by']
+const ATTRIBUTES = ['year', 'geoid', 'hazard', 'group_by'];
+const allowedHaz = ["wind", "wildfire", "tsunami", "tornado", "riverine", "lightning", "landslide", "icestorm", "hurricane",
+    "heatwave", "hail", "earthquake", "drought", "avalanche", "coldwave", "winterweat", "volcano", "coastal"];
+
+const Fetch = (falcor, attr) => {
+    React.useEffect(() => {
+        function fetchData() {
+            falcor.get(
+                ['swdOfdMerge', 'indexValues', ['geoid', 'hazard', 'year']],
+                ['swdOfdMerge', 'swd', attr],
+                ['swdOfdMerge', 'swd', 'hazard.year'],
+                ['swdOfdMerge', 'swd', 'geoid.hazard.year'],
+                ['swdOfdMerge', 'ofd', attr],
+                ['swdOfdMerge', 'ofd', 'hazard.year'],
+                ['swdOfdMerge', 'ofd', 'geoid.hazard.year'],
+            )
+        }
+
+        return fetchData()
+    }, [attr, falcor])
+}
+
+const Process = (falcorCache, attr) => {
+    return React.useMemo(() => {
+        return {
+            swd: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'swd', attr, 'value'], []), attr),
+            swdByHazByYear: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'swd', 'hazard.year', 'value'], [])),
+            swdByGeoByHazByYear: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'swd', 'geoid.hazard.year', 'value'], [])),
+
+            ofd: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'ofd', attr, 'value'], []), attr),
+            ofdByHazByYear: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'ofd', 'hazard.year', 'value'], [])),
+            ofdByGeoByHazByYear: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'ofd', 'geoid.hazard.year', 'value'], [])),
+
+            indexValues: {
+                hazard: get(falcorCache, ['swdOfdMerge', 'indexValues', 'hazard', 'value'], []),
+                year: get(falcorCache, ['swdOfdMerge', 'indexValues', 'year', 'value'], []),
+                geoid: get(falcorCache, ['swdOfdMerge', 'indexValues', 'geoid', 'value'], [])
+            }
+        }
+    }, [falcorCache, attr])
+}
 
 const convertDataToNumeric = (data, index) => {
     return data.map(d => {
@@ -17,85 +58,171 @@ const convertDataToNumeric = (data, index) => {
     })
 }
 
-const mergeData = ({swd, ofd, index, indexValues}) => {
-    let result = [];
+const MergeData = ({swd, ofd, index, indexValues}) => {
+    return React.useMemo(() => {
+        let result = [];
 
-    indexValues[index].forEach((iv, i) => {
-        result.push({
-            [index]: iv,
-            swd_loss: get(swd.filter(s => s[index] === iv), [0, 'total_damage'], 0),
-            ofd_loss: get(ofd.filter(o => o[index] === iv), [0, 'total_loss'], 0)
-        })
-    })
-    return result;
-}
-
-const mergeDataDeep = (swd, ofd, index, indexValues) => {
-    let result = [];
-
-    indexValues[index[0]].forEach((iv, i) => {
-        indexValues[index[1]].forEach((iv1, i1) => {
+        indexValues[index].forEach((iv, i) => {
             result.push({
-                [index[0]]: iv,
-                [index[1]]: iv1,
-                swd_loss: get(swd.filter(s => s[index[0]] === iv && s[index[1]] === iv1), [0, 'total_damage'], 0),
-                ofd_loss: get(ofd.filter(o => o[index[0]] === iv && o[index[1]] === iv1), [0, 'total_loss'], 0)
+                [index]: iv,
+                swd_loss: get(swd.filter(s => s[index] === iv), [0, 'total_damage'], 0),
+                ofd_loss: get(ofd.filter(o => o[index] === iv), [0, 'total_loss'], 0)
             })
         })
-    })
-    return result;
+        return result;
+    }, [indexValues, index, swd, ofd])
+}
+
+const MergeDataDeep = (swd, ofd, index, indexValues) => {
+    return React.useMemo(() => {
+        let result = [];
+
+        if(index.length === 2){
+            indexValues[index[0]].forEach((iv, i) => {
+                indexValues[index[1]].forEach((iv1, i1) => {
+                    result.push({
+                        [index[0]]: iv,
+                        [index[1]]: iv1,
+                        swd_loss: get(swd.filter(s => s[index[0]] === iv && s[index[1]] === iv1), [0, 'total_damage'], 0),
+                        ofd_loss: get(ofd.filter(o => o[index[0]] === iv && o[index[1]] === iv1), [0, 'total_loss'], 0)
+                    })
+                })
+            })
+        } else if (index.length === 3){
+            indexValues[index[0]].forEach((iv, i) => {
+                indexValues[index[1]].forEach((iv1, i1) => {
+                    indexValues[index[2]].forEach((iv2, i2) => {
+                        result.push({
+                            [index[0]]: iv,
+                            [index[1]]: iv1,
+                            [index[2]]: iv2,
+                            swd_loss: get(swd.filter(s => s[index[0]] === iv && s[index[1]] === iv1 && s[index[2]] === iv2), [0, 'total_damage'], 0),
+                            ofd_loss: get(ofd.filter(o => o[index[0]] === iv && o[index[1]] === iv1 && o[index[2]] === iv2), [0, 'total_loss'], 0)
+                        })
+                    })
+                })
+            })
+        }
+
+        return result;
+    }, [index, indexValues, ofd, swd])
+}
+
+const renderTabs = (tabs, setView, classNames) => {
+    return (
+        <div>
+            <div className="sm:hidden">
+                <label htmlFor="tabs" className="sr-only">
+                    Select a tab
+                </label>
+                <select
+                    id="tabs"
+                    name="tabs"
+                    className="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                    defaultValue={tabs.find((tab) => tab.current).name}
+                    onChange={e => setView(e.target.value)}
+                >
+                    {tabs.map((tab) => (
+                        <option key={tab.name}>{tab.name}</option>
+                    ))}
+                </select>
+            </div>
+
+
+            <div className="hidden sm:block">
+                <div className="border-b border-gray-200">
+                    <nav className="-mb-px flex" aria-label="Tabs">
+                        {tabs.map((tab) => (
+                            <a
+                                key={tab.name}
+                                href={tab.href}
+                                className={classNames(
+                                    tab.current
+                                        ? 'border-indigo-500 text-indigo-600'
+                                        : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                                    'w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm'
+                                )}
+                                aria-current={tab.current ? 'page' : undefined}
+                                onClick={e => setView(tab.name)}
+                            >
+                                {tab.name}
+                            </a>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const renderChart = (merged, mergedByHazardByYear, attr) => {
+    return (
+        <>
+            <div className='pt-4 pb-3 px-4 bg-white'>
+                <div className='p-2' style={{height: '500px'}}>
+                    {merged.length ? <BarGraph
+                        data={merged}
+                        keys={['swd_loss', 'ofd_loss']}
+                        indexBy={attr}
+                        axisBottom={d => d}
+                        axisLeft={{format: fnum}}
+                        indexFormat={fnum}
+                        valueFormat={fnum}
+                        groupMode={'grouped'}
+                        colors={['#6ee173', '#5f78c9']}
+                    /> : null}
+                </div>
+            </div>
+
+            <div className="pt-5 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
+                {
+                    allowedHaz
+                        .map(hazard => (
+                            <div className='pt-4 pb-3 px-2 bg-white'>
+                                <label className={'font-bold'}>  {hazard} </label>
+                                <div className='p-2' style={{height: '300px'}}>
+                                    {mergedByHazardByYear.length ? <BarGraph
+                                        data={mergedByHazardByYear.filter(m => m.hazard === hazard)}
+                                        keys={['swd_loss', 'ofd_loss']}
+                                        indexBy={'year'}
+                                        axisBottom={d => d}
+                                        axisLeft={{format: fnum}}
+                                        indexFormat={fnum}
+                                        valueFormat={fnum}
+                                        groupMode={'grouped'}
+                                        colors={['#6ee173', '#5f78c9']}
+                                    /> : null}
+                                </div>
+                            </div>
+
+                        ))
+                }
+            </div>
+        </>
+    )
 }
 
 const Merge = (props) => {
+    const [view, setView] = React.useState('Map');
     const {falcor, falcorCache} = useFalcor();
     const attr = 'year'
-    React.useEffect(() => {
-        function fetchData() {
-            falcor.get(
-                ['swdOfdMerge', 'indexValues', ['hazard', 'year']],
-                ['swdOfdMerge', 'swd', attr],
-                ['swdOfdMerge', 'swd', 'hazard.year'],
-                ['swdOfdMerge', 'ofd', attr],
-                ['swdOfdMerge', 'ofd', 'hazard.year'],
-            )
-        }
-        return fetchData()
-    })
-    const data = React.useMemo(() => {
 
-        return {
-            swd: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'swd', attr, 'value'], []), attr),
-            swdByHazByYear: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'swd', 'hazard.year', 'value'], [])),
-            ofd: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'ofd', attr, 'value'], []), attr),
-            ofdByHazByYear: convertDataToNumeric(get(falcorCache, ['swdOfdMerge', 'ofd', 'hazard.year', 'value'], [])),
-            indexValues: {
-                hazard: get(falcorCache, ['swdOfdMerge', 'indexValues', 'hazard', 'value'], []),
-                year: get(falcorCache, ['swdOfdMerge', 'indexValues', 'year', 'value'], [])
-            }
-        }
-    }, [falcorCache])
+    Fetch(falcor, attr)
 
-    const merged = React.useMemo(() => {
-        return mergeData({...data, index:attr})
-    }, [data])
+    const data = Process(falcorCache, attr)
 
-    const mergedByHazardByYear = React.useMemo(() => {
-        return mergeDataDeep( data.swdByHazByYear, data.ofdByHazByYear, ['hazard', 'year'], data.indexValues)
-            // .filter(f => f.swd_loss !== 0 || f.ofd_loss !== 0)
-    }, [data])
+    const merged = MergeData({...data, index: attr})
 
+    const mergedByHazardByYear = MergeDataDeep(data.swdByHazByYear, data.ofdByHazByYear, ['hazard', 'year'], data.indexValues)
 
-    // const sortedHaz = data.indexValues["hazard"]
-    //     .sort((a,b) =>
-    //         mergedByHazardByYear.filter(m => m.hazard === b && (m.swd_loss !== 0 || m.ofd_loss !== 0)).length -
-    //         mergedByHazardByYear.filter(m => m.hazard === a && (m.swd_loss !== 0 || m.ofd_loss !== 0)).length
-    //     )
-    // console.log(sortedHaz.map(h => `'${h}'`).join(','))
+    // const mergedByGeoByHazardByYear = MergeDataDeep(data.swdByGeoByHazByYear, data.ofdByGeoByHazByYear, ['geoid', 'hazard', 'year'], data.indexValues, true)
 
-    const sortedHaz = ['tornado','hail','wind','coldwave','Dense Fog','Heavy Rain','hurricane','icestorm','lightning',
-        'riverine','wildfire','winterweat','coastal','drought','Dust Storm','Dust Devil','landslide','avalanche',
-        'heatwave','tsunami','Freezing Fog','earthquake','Dense Smoke','volcano','Astronomical Low Tide','Dam/Levee Break',
-        'Marine Dense Fog','Northern Lights','OTHER']
+    const tabs = [{ name: 'Chart', href: '#', current: view === 'Chart' },
+        { name: 'Map', href: '#', current: view === 'Map' }];
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
+    }
     return (
         <AdminLayout>
             <div className="w-full max-w-7xl mx-auto">
@@ -103,46 +230,16 @@ const Merge = (props) => {
                     <h3 className='inline font-bold text-3xl'>Merge</h3>
                 </div>
 
-                <div className='pt-4 pb-3 px-4 bg-white'>
-                    <div className='p-2' style={{height:'500px'}}>
-                        {merged.length ? <BarGraph
-                            data={merged}
-                            keys={['swd_loss', 'ofd_loss']}
-                            indexBy={attr}
-                            axisBottom={d => d}
-                            axisLeft={{format: fnum}}
-                            indexFormat={fnum}
-                            valueFormat={fnum}
-                            groupMode={'grouped'}
-                            colors={['#6ee173', '#5f78c9']}
-                        /> : null}
-                    </div>
+                {renderTabs(tabs, setView, classNames)}
+
+                <div className={view === 'Chart' ? 'block' : 'hidden'}>
+                    {renderChart(merged, mergedByHazardByYear, attr)}
                 </div>
 
-                <div className="pt-5 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
-                    {
-                        sortedHaz
-                            .map(hazard => (
-                                <div className='pt-4 pb-3 px-2 bg-white'>
-                                    <label className={'font-bold'}>  {hazard} </label>
-                                    <div className='p-2' style={{height:'300px'}}>
-                                        {mergedByHazardByYear.length ? <BarGraph
-                                            data={mergedByHazardByYear.filter(m => m.hazard === hazard)}
-                                            keys={['swd_loss', 'ofd_loss']}
-                                            indexBy={'year'}
-                                            axisBottom={d => d}
-                                            axisLeft={{format: fnum}}
-                                            indexFormat={fnum}
-                                            valueFormat={fnum}
-                                            groupMode={'grouped'}
-                                            colors={['#6ee173', '#5f78c9']}
-                                        /> : null}
-                                    </div>
-                                </div>
-
-                            ))
-                    }
+                <div className={view !== 'Chart' ? 'block' : 'hidden'}>
+                    {RenderMap(data)}
                 </div>
+
             </div>
         </AdminLayout>
     )
