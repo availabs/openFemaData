86eab45ce81c959b1b89a40811663d8f1821731b
shaunaksangdod <shaunaksangdod@gmail.com>
2021-10-05 16:59:41 -0400

    new tab

--- tasks/severe-weather-dataloader/postUploadProcessing.js
+++ tasks/severe-weather-dataloader/postUploadProcessing.js
@@ -139,6 +139,48 @@ const updateGeoMzone = async () => {
     return db.query(query);
 }
 
+const updateDateTime = async () => {
+    let query = `
+        UPDATE severe_weather_new.details
+        SET begin_date_time =
+                (LEFT(begin_yearmonth::text, 4) || '-' || RIGHT(begin_yearmonth::text, 2) || '-' || begin_day::text ||
+                 ' ' ||
+                 LPAD(LEFT(begin_time::text, LENGTH(begin_time::text) - 2), 2, '0') || ':' || RIGHT(begin_time::text, 2)
+                    )::TIMESTAMP,
+            end_date_time   =
+                (LEFT(end_yearmonth::text, 4) || '-' || RIGHT(end_yearmonth::text, 2) || '-' || end_day::text || ' ' ||
+                 LPAD(LEFT(end_time::text, LENGTH(end_time::text) - 2), 2, '0') || ':' || RIGHT(end_time::text, 2)
+                    )::TIMESTAMP
+        where extract(YEAR from begin_date_time) > 2021
+    `
+    return db.query(query);
+}
+
+const swdToDNMapping = async () => {
+    let query = `
+        TRUNCATE severe_weather_new.disaster_number_to_event_id_mapping;
+        with t as (
+            SELECT a.disaster_number,
+                   a.incident_type                                   disaster_type,
+                   ARRAY_AGG(fips_state_code || fips_county_code)    counties,
+                   MIN(incident_begin_date)                          incident_begin_date,
+                   MAX(incident_end_date)                            incident_end_date
+            FROM open_fema_data.disaster_declarations_summaries_v2 a
+            GROUP BY 1, 2)
+
+        INSERT INTO severe_weather_new.disaster_number_to_event_id_mapping
+        SELECT distinct disaster_number, event_id
+        FROM severe_weather_new.details sw
+                 JOIN t
+                      ON t.counties @> string_to_array(substring(geoid, 1, 5), ',')
+                          AND begin_date_time >= incident_begin_date
+                          AND end_date_time <= incident_end_date
+                          AND lower(event_type_formatted) = lower(disaster_type)
+        order BY disaster_number
+    `
+    return db.query(query);
+}
+
 const postProcess = async () => {
     // await updateCoords();
     // await updateDamage();
@@ -148,6 +190,8 @@ const postProcess = async () => {
     // await removeGeoidZzone();
     // await updateGeoZzone();
     // await updateGeoMzone();
+    // await updateDateTime();
+    // await swdToDNMapping();
 }
 
 postProcess()
