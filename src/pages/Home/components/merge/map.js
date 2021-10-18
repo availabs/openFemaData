223cc017c7cedbd01c4dab20fc236c7e1c9a3449
shaunaksangdod <shaunaksangdod@gmail.com>
2021-10-18 17:01:44 -0400

    map by disaster #

--- src/pages/Home/components/merge/map.js
+++ src/pages/Home/components/merge/map.js
@@ -2,9 +2,13 @@ import React from 'react'
 import {AvlMap} from '@availabs/avl-map'
 import {MAPBOX_TOKEN} from "mapboxConfig";
 import {MergeDataFactory} from "components/layers/mergeData";
+import {MergeDataByDNFactory} from "components/layers/mergeDataByDN";
 
 export const RenderMap = (data) => {
-    const Layers = React.useRef([MergeDataFactory()]);
+    const Layers = React.useRef([
+        // MergeDataFactory(),
+        MergeDataByDNFactory()
+    ]);
 
     return (
         <div className='flex-1 flex flex-col pt-5 shadow-lg' style={{height: '800px'}}>
